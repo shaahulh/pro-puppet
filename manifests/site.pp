41bc034ce4ebdb7eabca815f710b1f43840e6ba9
Shaahul Hameed <shaahul21@gmail.com>
2014-08-08 17:15:14 +1000

    pro-puppet Chapter 1

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,3 @@
 node 'sha-node1.pro-puppet.com' {
-	package { 'vim':
-		ensure => present,
-	}
-	package { 'finger':
-		ensure => absent,
-	}
+	include	sudo
 }	
--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -3,19 +3,21 @@ class sudo {
 	package { 'sudo':
 		ensure => present,
 	}
- 
+
 	if $::osfamily == 'Debian' {
 		package { 'sudo-ldap':
 			ensure => present,
 			require => Package['sudo'],
-		}
+			}
 	}
- 
+
 	file { '/etc/sudoers':
 		owner => 'root',
 		group => 'root',
-		mode => '0440'
+		mode => '0440',
 		source => "puppet://$::server/modules/sudo/etc/sudoers",
 		require => Package['sudo'],
 	}
+
+ 
 }
